7adc7bc4c1e9b90eb972af266ce463e2763314f4
Leandro Ekamoto <Leandro Ekamoto>
2018-02-21 09:14:53 -0400

    Add DataAnnotations: MaxLength e Range to decimal

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -7,11 +7,13 @@ namespace AspNetCoreMVC.Models
     {
         [Required]
         public int Id { get; set; }
-        
+
         [Required]
+        [MaxLength(10)]
         public string Name { get; set; }
 
         [Required]
-        public decimal Price { get; set; }
+        [Range(1, 9999999999999999.99)]
+        public Decimal Price { get; set; }
     }
 }
